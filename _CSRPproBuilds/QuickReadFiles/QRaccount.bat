cd1d0d9286c4be0240d1956a6d1ea9d089d1b8b1
13 Devils Mod <52423009+13DM@users.noreply.github.com>
2019-09-30 16:53:56 -0500

    Update QRaccount.bat

--- _CSRPproBuilds/QuickReadFiles/QRaccount.bat
+++ _CSRPproBuilds/QuickReadFiles/QRaccount.bat
@@ -2,10 +2,19 @@
 
 cls
 
-call QRStats.bat .\nsb.txt
+call ".\QRStats.bat" ".\SessionInfo"
 
 cls
 
-type AccountPreview
-
+cls
+echo   ____        _      __      ____                 __   _   _______ ____ 
+echo  / __ \__  __(_)____/ /__   / __ \___  ____ _____/ /  / ^| / / ___// __ )
+echo / / / / / / / / ___/ //_/  / /_/ / _ \/ __ `/ __  /  /  ^|/ /\__ \/ __  ^|
+echo/ /_/ / /_/ / / /__/ ,^<    / _, _/  __/ /_/ / /_/ /  / /^|  /___/ / /_/ / 
+echo\___\_\__,_/_/\___/_/^|_^|  /_/ ^|_^|\___/\__,_/\__,_/  /_/ ^|_//____/_____/  
+echo.                                                                         
+echo.
+echo File Contents:
+echo.
+type ".\AccountPreview"
 pause >nul
